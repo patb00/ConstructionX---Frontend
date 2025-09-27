dc3544afe6ef448b2f929045346cb1515a0f4275
patb00 <bozic.patrik15@gmail.com>
2025-09-27 21:26:42 +0200

    fix loading main screen

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,7 @@ export default defineConfig({
       "/api": {
         target: "http://fradotovic-001-site1.jtempurl.com",
         changeOrigin: true,
-        secure: false, // allow self-signed HTTPS
-        // rewrite: (path) => path, // keep /api prefix
+        secure: false,
       },
     },
   },
